e3188ebb07f067640ef547db4fca792ab6d03f7b
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-08 10:39:20 +0100

    Reactjs

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 function Counter() {
     const [count, setCount] = useState(0)
     const add = () => {
-     setCount(prevState => prevState - 1)
+     setCount(count + 1)
     }
-    const subtract = () => {}
+    const subtract = () => {
+        setCount(count - 1)
+    }
   return (
     <section className="Counter">
       <h1>{count}</h1>
