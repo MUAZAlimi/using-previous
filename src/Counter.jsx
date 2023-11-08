867fffd4281e1e3d8a8f9f5146b935f343ebe0a5
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-08 10:38:12 +0100

    Reactjs

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 
 function Counter() {
     const [count, setCount] = useState(0)
+    const add = () => {
+     setCount(prevState => prevState - 1)
+    }
+    const subtract = () => {}
   return (
     <section className="Counter">
       <h1>{count}</h1>
       <div className="calcBtn">
-        <button onClick={() => setCount(prevState => prevState + 1)}>+</button>
-        <button onClick={() => setCount(prevState => prevState - 1)}>-</button>
+        <button onClick={add}>+</button>
+        <button onClick={subtract}>-</button>
       </div>
         <div>
         <button onClick={() => setCount(0)}>Reset</button>
