79eba0b90c2c2bcc6a8dddbad08e512e492ff9a9
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-08 10:26:20 +0100

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0)
+  return (
+    <section className="Counter">
+      <h1>{count}</h1>
+      <div className="calcBtn">
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+      </div>
+        <div>
+        <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+    </section>
+  )
+}
+
+export default Counter
